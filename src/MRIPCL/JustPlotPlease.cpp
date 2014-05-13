ef618d8ac5b3f063f0ef990e08a490c22c6a1208
mri <mri@mri-OptiPlex-755.(none)>
2014-05-13 01:17:54 -0700

    This room smells weird and it's 1:18. Time to watch some GOT

---
+++ src/MRIPCL/JustPlotPlease.cpp
@@ -0,0 +1,54 @@
+/*
+ * JustPlotPlease.cpp
+ *
+ *  Created on: May 12, 2014
+ *      Author: mri
+ */
+
+#include "JustPlotPlease.h"
+
+JustPlotPlease::JustPlotPlease() {
+	// TODO Auto-generated constructor stub
+	this->viewer = new pcl::visualization::PCLVisualizer ("Point Cloud Viewrzz ;) Hi Chris");
+    this->clouds = new std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>(500);
+
+}
+
+JustPlotPlease::~JustPlotPlease() {
+	// TODO Auto-generated destructor stub
+}
+
+void JustPlotPlease::PlusCloud(pcl::PointCloud<pcl::PointXYZ> * cloudnine, int id, unsigned char r, unsigned char g, unsigned char b) {
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(cloudnine);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_xyzrgb (new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::copyPointCloud(*cloud_in, *cloud_xyzrgb);
+	for (size_t i = 0; i < cloud_xyzrgb->points.size(); ++i) {
+			cloud_xyzrgb->points[i].r = r;
+			cloud_xyzrgb->points[i].g = g;
+			cloud_xyzrgb->points[i].b = b;
+	}
+	this->clouds->at(id) = cloud_xyzrgb;
+}
+
+void JustPlotPlease::ShowCloud(int id) {
+	viewer->setBackgroundColor(0, 0, 0);
+	pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(this->clouds->at(id));
+	viewer->addPointCloud<pcl::PointXYZRGB>((this->clouds->at(id)), rgb, "image");
+
+	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "image");
+
+	viewer->addCoordinateSystem(1, 0, 0, 100);
+
+	viewer->initCameraParameters();
+
+	viewer->spin();
+
+	while (!viewer->wasStopped()) {
+		double x, y, z, r, b, o;
+		cout << "give x y  z \n";
+		cin >> x;
+		cin >> y;
+		cin >> z;
+		viewer->setCameraPosition(x, y, z, 0, 0, 0, 0);
+	}
+}
---
+++ src/MRIPCL/JustPlotPlease.h
@@ -0,0 +1,33 @@
+/*
+ * JustPlotPlease.h
+ *
+ *  Created on: May 12, 2014
+ *      Author: mri
+ */
+
+#ifndef JustPlotPlease_H_
+#define JustPlotPlease_H_
+
+#include <vector>
+#include <LabeledResults.h>
+#include <iostream>
+#include "MRIPCLProcess.h"
+#include <MRIProcess.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <boost/thread/thread.hpp>
+#include <pcl/filters/voxel_grid.h>
+#include <ctype.h>
+
+class JustPlotPlease {
+public:
+	JustPlotPlease();
+	virtual ~JustPlotPlease();
+	void PlusCloud(pcl::PointCloud<pcl::PointXYZ> * cloudnine, int id, unsigned char r, unsigned char g, unsigned char b);
+	void ShowCloud(int id);
+	pcl::visualization::PCLVisualizer * viewer;
+	std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> * clouds;
+};
+
+#endif /* JustPlotPlease_H_ */
