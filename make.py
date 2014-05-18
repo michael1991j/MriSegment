88045ae46e6a7693393b2c4d3cf61c924e48f303
mri <mri@mri-OptiPlex-755.(none)>
2014-05-18 11:03:00 -0700

    Added -j2 to make in python script

--- make.py
+++ make.py
@@ -33,7 +33,7 @@ if selection == 0:
 		os.system('mkdir -p ' + file)
 		os.chdir(file)
 		os.system('cmake ' + top + '/src/' + file)
-		os.system('make')
+		os.system('make -j2')
 		os.system('make install')
 		os.chdir(top + '/Build')
 
